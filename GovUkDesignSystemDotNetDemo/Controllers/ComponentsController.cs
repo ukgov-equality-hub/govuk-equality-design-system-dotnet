fb7912e3c2f1489f55c0c1853026707174838dfa
James Griffiths <james@jwgsoftware.com>
2025-07-09 18:04:02 +0100

    Demo - example framework and empty components list page

---
+++ GovUkDesignSystemDotNetDemo/Controllers/ComponentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GovUkDesignSystemDotNetDemo.Controllers;
+
+[Route("components")]
+public class ComponentsController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpGet("examples/{exampleName}")]
+    public IActionResult ExamplePage(string exampleName)
+    {
+        return View(exampleName);
+    }
+}
