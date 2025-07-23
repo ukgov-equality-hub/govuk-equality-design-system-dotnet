754eb969697233af82733ab52dc0e37a599523a3
James Griffiths <james@jwgsoftware.com>
2025-07-23 10:49:00 +0100

    Back link

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/BackLinkViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class BackLinkViewModel
+{
+    public HtmlOrText HtmlOrText { get; set; }
+    public string Href { get; set; }
+    public bool OverrideWithJavascript { get; set; }
+    public string Classes { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+}
