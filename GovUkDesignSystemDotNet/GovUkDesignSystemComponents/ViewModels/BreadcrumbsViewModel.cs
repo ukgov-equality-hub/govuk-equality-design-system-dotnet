423fb490d8ef523b713073dc9a0f7e21f63ecee1
James Griffiths <james@jwgsoftware.com>
2025-07-23 10:48:19 +0100

    Breadcrumbs

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/BreadcrumbsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class BreadcrumbsViewModel
+{
+    public List<CrumbViewModel> Crumbs { get; set; } = [];
+    public List<string> Classes { get; set; } = [];
+    public bool CollapseOnMobile { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+    public string LabelText { get; set; }
+}
+
+public class CrumbViewModel
+{
+    public HtmlOrText HtmlOrText { get; set; }
+    public string Href { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+}
