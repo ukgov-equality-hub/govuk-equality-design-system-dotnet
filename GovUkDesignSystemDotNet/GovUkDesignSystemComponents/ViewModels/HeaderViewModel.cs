9dff8d9d416c2f4702cb79ff73123fe34ce6ab72
James Griffiths <james@jwgsoftware.com>
2025-07-23 10:50:09 +0100

    Header

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class HeaderViewModel
+{
+    public string HomepageUrl { get; set; }
+    public string ProductName { get; set; }
+    public List<string> ContainerClasses { get; set; } = [];
+    public List<string> Classes { get; set; } = [];
+    public Dictionary<string, string> Attributes { get; set; }
+}
