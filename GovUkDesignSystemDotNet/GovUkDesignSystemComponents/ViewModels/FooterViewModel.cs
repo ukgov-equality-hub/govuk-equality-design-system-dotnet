f20054867fcbc00f17ea811b394ce30909964a08
James Griffiths <james@jwgsoftware.com>
2025-07-23 13:21:18 +0100

    Footer

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/FooterViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class FooterViewModel
+{
+    public FooterMetaViewModel Meta { get; set; }
+    public List<FooterNavigationSectionViewModel> NavigationSections { get; set; } = [];
+    public HtmlOrText ContentLicence { get; set; }
+    public HtmlOrText Copyright { get; set; }
+    public List<string> ContainerClasses { get; set; } = [];
+    public List<string> Classes { get; set; } = [];
+    public Dictionary<string, string> Attributes { get; set; }
+}
+
+public class FooterMetaViewModel {
+    public string VisuallyHiddenTitle { get; set; }
+    public HtmlOrText HtmlOrText { get; set; }
+    public List<FooterItemViewModel> Items { get; set; } = [];
+}
+
+public class FooterNavigationSectionViewModel {
+    public string Title { get; set; }
+    public int? Columns { get; set; }
+    public string Width { get; set; }
+    public List<FooterItemViewModel> Items { get; set; } = [];
+}
+
+public class FooterItemViewModel {
+    public string Text { get; set; }
+    public string Href { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+}
