8656fef7d2e8a0b9c5a37cf9d06c169de504f346
James Griffiths <james@jwgsoftware.com>
2025-07-28 21:20:37 +0100

    Error message

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/ErrorMessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class ErrorMessageViewModel
+{
+    public HtmlOrText HtmlOrText { get; set; }
+    public string Id { get; set; }
+    public List<string> Classes { get; set; } = [];
+    public Dictionary<string, string> Attributes { get; set; }
+    public string VisuallyHiddenText { get; set; }
+}
--- GovUkDesignSystemDotNetDemo/Controllers/ComponentsController.cs
+++ GovUkDesignSystemDotNetDemo/Controllers/ComponentsController.cs
@@ -17,6 +17,9 @@ public class ComponentsController : Controller
     [HttpGet("button")]
     public IActionResult Button() => View();
 
+    [HttpGet("error-message")]
+    public IActionResult ErrorMessage() => View();
+
     [HttpGet("footer")]
     public IActionResult Footer() => View();
 
