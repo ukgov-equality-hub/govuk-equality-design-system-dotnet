c94735af58a547553d30edefef424973898dbe19
James Griffiths <james@jwgsoftware.com>
2025-08-04 18:10:58 +0100

    Fieldset

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/FieldsetViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class FieldsetViewModel
+{
+    public List<string> DescribedBy { get; set; } = [];
+    public FieldsetLegendViewModel Legend { get; set; }
+    public List<string> Classes { get; set; } = [];
+    public string Role { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+    public HtmlOrText HtmlOrText { get; set; }
+}
+
+public class FieldsetLegendViewModel
+{
+    public HtmlOrText HtmlOrText { get; set; }
+    public List<string> Classes { get; set; } = [];
+    public bool IsPageHeading { get; set; }
+}
