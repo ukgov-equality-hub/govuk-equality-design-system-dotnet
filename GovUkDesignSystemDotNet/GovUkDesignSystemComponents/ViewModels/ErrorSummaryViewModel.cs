7e37a48396396b322938af787ff209ab86838046
James Griffiths <james@jwgsoftware.com>
2025-08-06 17:35:45 +0100

    Error summary

---
+++ GovUkDesignSystemDotNet/GovUkDesignSystemComponents/ViewModels/ErrorSummaryViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace GovUkDesignSystemDotNet;
+
+public class ErrorSummaryViewModel
+{
+    public HtmlOrText Title { get; set; }
+    public HtmlOrText Description { get; set; }
+    public List<ErrorSummaryItemViewModel> Errors { get; set; } = [];
+    public bool DisableAutoFocus { get; set; }
+    public List<string> Classes { get; set; } = [];
+    public Dictionary<string, string> Attributes { get; set; }
+}
+
+public class ErrorSummaryItemViewModel
+{
+    public string Href { get; set; }
+    public HtmlOrText HtmlOrText { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+}
