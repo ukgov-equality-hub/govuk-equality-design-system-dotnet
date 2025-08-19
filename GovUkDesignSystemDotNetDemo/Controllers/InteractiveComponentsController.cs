64543eed5c55c9d431266ea5b8438f0231290cac
James Griffiths <james@jwgsoftware.com>
2025-08-19 15:57:59 +0100

    Demo - Interactive

---
+++ GovUkDesignSystemDotNetDemo/Controllers/InteractiveComponentsController.cs
@@ -0,0 +1,36 @@
+using GovUkDesignSystemDotNetDemo.Models.InteractiveComponentsExamples;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUkDesignSystemDotNetDemo.Controllers;
+
+[Route("interactive-components")]
+public class InteractiveComponentsController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index() => View();
+
+    
+    private IActionResult ExampleDefault<TViewModel>(TViewModel viewModel)
+    where TViewModel : new()
+    {
+        string actionName = ControllerContext.RouteData.Values["action"].ToString();
+        
+        switch (Request.Method)
+        {
+            case "GET":
+                ModelState.Clear();
+                return View(actionName, new TViewModel());
+
+            case "POST":
+                if (!ModelState.IsValid)
+                {
+                    return View(actionName, viewModel);
+                }
+
+                return View("InteractiveExampleValidationSuccessful");
+            
+            default:
+                return StatusCode(StatusCodes.Status405MethodNotAllowed);
+        }
+    }
+}
---
+++ GovUkDesignSystemDotNetDemo/Models/InteractiveExampleViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GovUkDesignSystemDotNetDemo.Models;
+
+public class InteractiveExampleViewModel
+{
+    public string ExampleHref { get; set; }
+    public List<InteractiveExampleCodeSnippetViewModel> CodeSnippets { get; set; } = [];
+}
+
+public class InteractiveExampleCodeSnippetViewModel
+{
+    public string Title { get; set; }
+    public string Filename { get; set; }
+    public string Code { get; set; }
+}
